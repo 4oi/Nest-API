4df6eac0baac2ee5def265c991976312ae8f762e
toyblocks <a@llv.jp>
2016-07-17 03:39:12 +0900

    Move type registration into core

--- src/main/java/jp/llv/nest/command/obj/bind/TypeBinding.java
+++ src/main/java/jp/llv/nest/command/obj/bind/TypeBinding.java
@@ -5,18 +5,9 @@ package jp.llv.nest.command.obj.bind;
 
 import jp.llv.nest.command.exceptions.UndefinedVariableException;
 import jp.llv.nest.command.exceptions.UnmodifiableVariableException;
-import jp.llv.nest.command.obj.NestBool;
-import jp.llv.nest.command.obj.NestCommandSender;
-import jp.llv.nest.command.obj.NestDecimal;
-import jp.llv.nest.command.obj.NestHash;
-import jp.llv.nest.command.obj.NestInt;
-import jp.llv.nest.command.obj.NestList;
 import jp.llv.nest.command.obj.NestObject;
 import jp.llv.nest.command.obj.NestObjectAdapter;
-import jp.llv.nest.command.obj.NestPermitter;
-import jp.llv.nest.command.obj.NestString;
 import jp.llv.nest.command.obj.NestType;
-import jp.llv.nest.command.obj.PairedValue;
 
 /**
  *
@@ -27,17 +18,6 @@ public abstract class TypeBinding extends NestObjectAdapter<Void> implements Bin
     /*package*/ static TypeBinding INSTANCE;
     
     /*package*/ TypeBinding() {
-        this.registerClass(NestBool.class, 
-                NestCommandSender.class, 
-                NestDecimal.class, 
-                NestHash.class,
-                NestInt.class,
-                NestList.class,
-                NestObject.class,
-                NestPermitter.class,
-                NestString.class,
-                NestType.class,
-                PairedValue.class);
     }
 
     @Override
